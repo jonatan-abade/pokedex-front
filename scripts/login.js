7645902d8cf5056a4612c7ffc3539f94070589ea
Gabriel Abade <161393183+gabrielvitorabade@users.noreply.github.com>
2024-11-08 15:48:36 -0300

    adição de funcionalidades de validação de login.

--- scripts/login.js
+++ scripts/login.js
@@ -1,3 +1,20 @@
-function fazerValidacaoLogin(){
-    window.alert('deu certo')
+
+function visibilidade() {
+    let senha = document.getElementById('password')
+    let icone = document.querySelector('img#iconeVisibilidade')
+    if (senha.type == 'text') {
+        senha.type = 'password'
+        icone.src = "../imagens/iconeVisivel.png"
+    } else {
+        senha.type = 'text'
+        icone.src = "../imagens/iconeInvisivel.png"
+    }
 }
+function fazerValidacaoLogin() {
+    let login = document.querySelector('input#name').value;
+    let senha = document.querySelector('input#password').value;
+    console.log(login, senha);
+
+}
+
+
